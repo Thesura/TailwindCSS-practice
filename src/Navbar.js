5e5a1741f75fd30d0b3664555e2300c8f9536170
Thesura Siriwardhana <thesuracom@gmail.com>
2024-02-13 13:35:56 +0530

    updated collapsed nav items to include theming

--- src/Navbar.js
+++ src/Navbar.js
@@ -44,10 +44,10 @@ const Navbar = () => {
       </div>
       <div className={`${toggled?'scale-100 transition-all duration-150 ease-linear':'scale-0'} fixed sm:hidden top-16 w-auto ${theme?'bg-emerald-950':'bg-sky-950'} text-gray-300 shadow-lg`}>
         <div className="nav-list">
-          <p className="nav-list-item">Home</p>
-          <p className="nav-list-item">Social</p>
-          <p className="nav-list-item">About</p>
-          <p className="nav-list-item">Contact Us</p>
+          <p className={`nav-list-item ${theme?'nav-list-item-emerald':'nav-list-item-sky'}`}>Home</p>
+          <p className={`nav-list-item ${theme?'nav-list-item-emerald':'nav-list-item-sky'}`}>Social</p>
+          <p className={`nav-list-item ${theme?'nav-list-item-emerald':'nav-list-item-sky'}`}>About</p>
+          <p className={`nav-list-item ${theme?'nav-list-item-emerald':'nav-list-item-sky'}`}>Contact Us</p>
         </div>
       </div>
     </>
